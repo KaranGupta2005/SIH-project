0e78f358c2a07a524826761e854e73f01a11a32b
Karan Gupta <guptakaranjune2005@gmail.com>
2025-09-03 18:00:46 +0530

    Navbar Added and Map UI improved

--- SIH/frontend/MysticSikkim/src/App.jsx
+++ SIH/frontend/MysticSikkim/src/App.jsx
@@ -1,16 +1,36 @@
-import { useState } from 'react' 
-import LiveMap from './components/MapMarker'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import VirtualTour from "./pages/VirtualTour";
+import Map from "./pages/Map";
+import Archives from "./pages/Archives";
+import Calendar from "./pages/Calendar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className="w-[50vw] h-[100vh] bg-amber-300">
-         <LiveMap className='h-[100vh]'/>
-      </div> 
-    </>
-  )
+    <BrowserRouter>
+      <div className="relative flex flex-col min-h-screen w-full overflow-x-hidden overflow-y-auto scroll-smooth">
+        
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Routes */}
+        <section className="flex-1 mt-28"> 
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/virtualTour" element={<VirtualTour />} />
+            <Route path="/exploreMap" element={<Map />} />
+            <Route path="/archives" element={<Archives />} />
+            <Route path="/calendar" element={<Calendar />} />
+          </Routes>
+        </section>
+
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
+
---
+++ SIH/frontend/MysticSikkim/src/pages/Archives.jsx
@@ -0,0 +1,8 @@
+export default function Archives() {
+  return (
+    <div>
+      <h1>Archives</h1>
+      <p>Welcome to the Archives page!</p>
+    </div>
+  );
+}
